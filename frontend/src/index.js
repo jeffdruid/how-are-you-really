19225a7381d0d49e1609dd0949af937f66b8f73d
JeffersonAguiar <jfdruida@gmail.com>
2024-10-22 00:14:40 +0100

    Refactor routing in index.js to use BrowserRouter for better navigation handling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const container = document.getElementById('root');
-const root = createRoot(container); 
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AuthProvider>
   </React.StrictMode>
 );
