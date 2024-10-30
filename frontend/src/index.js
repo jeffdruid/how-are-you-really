7c8a559f08e48043a44eecbbbbeb89977d304204
JeffersonAguiar <jfdruida@gmail.com>
2024-10-30 18:17:17 +0000

    Refactor index.js to use StrictMode and improve code organization; add comments for clarity

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
+    {/* Context Provider and Routing */}
     <AuthProvider>
       <Router>
         <App />
       </Router>
     </AuthProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
