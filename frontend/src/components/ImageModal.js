36ebd16214a19964eb3324796aa29c56e92bf031
JeffersonAguiar <jfdruida@gmail.com>
2024-11-02 01:07:27 +0000

    Enhance ImageModal component with improved styling, custom classes for transparency, and a background overlay for better user experience

--- frontend/src/components/ImageModal.js
+++ frontend/src/components/ImageModal.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Modal, Image } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Modal, Image } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-const ImageModal = ({ show, handleClose, imageUrl, altText = 'Image' }) => {
+const ImageModal = ({ show, handleClose, imageUrl, altText = "Image" }) => {
   return (
-    <Modal show={show} onHide={handleClose} centered size="lg">
-      <Modal.Body className="p-0">
-        <Image src={imageUrl} alt={altText} fluid />
+    <Modal
+      show={show}
+      onHide={handleClose}
+      centered
+      size="lg"
+      contentClassName="bg-transparent border-0" // Custom class for transparency
+      dialogClassName="image-modal-dialog"
+    >
+      <Modal.Body
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "80vh", backgroundColor: "rgba(0, 0, 0, 0)" }} // background overlay
+      >
+        <Image
+          src={imageUrl}
+          alt={altText}
+          fluid
+          style={{ maxHeight: "100%", maxWidth: "100%" }}
+        />
       </Modal.Body>
     </Modal>
   );
@@ -18,6 +33,5 @@ ImageModal.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   altText: PropTypes.string,
 };
-
 
 export default ImageModal;
